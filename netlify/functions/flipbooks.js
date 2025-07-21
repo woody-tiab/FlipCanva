684683917fa2662b4af79548c41122c0b5116bd2
woody-tiab <failan35@gmail.com>
2025-07-21 21:50:27 +0900

    Fix API URL to use Netlify Functions and remove uuid dependency

--- netlify/functions/flipbooks.js
+++ netlify/functions/flipbooks.js
@@ -1,5 +1,11 @@
 // Netlify Functions용 Flipbooks API 핸들러
-const { v4: uuidv4 } = require('uuid');
+// UUID 생성을 위한 간단한 함수
+const uuidv4 = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+};
 
 // 메모리 기반 임시 저장소 (실제로는 Supabase나 다른 DB 사용 권장)
 let flipbooks = [];
